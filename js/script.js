790a43e99b1713475abdee2e1698b02805114f58
Harald Pehl <harald.pehl@gmail.com>
2011-11-16 21:37:11 +0000

    Rewrote html pages. Removed unnecessary css resources. Added defaultvalue jQuery plugin

--- js/script.js
+++ js/script.js
@@ -200,3 +200,36 @@ function checkRequired()
     }
     return true;
 }
+
+function findRoute()
+{
+    var street = document.mapform.street.value;
+    var zip = document.mapform.zip.value;
+    var city = document.mapform.city.value;
+    
+    if (street == "Straße") street = "";
+    if (zip    == "PLZ")    zip    = "";
+    if (city   == "Ort")    city   = "";
+    
+    if (street == "" && zip == "" && city == "")
+    {
+        alert("Bitte geben Sie eine gültige Adresse ein.");
+    }
+    else
+    {
+        var from = "?";
+        if (street != "")
+        {
+            from += street + ", ";
+        }
+        if (zip != "")
+        {
+            from += zip + " ";
+        }
+        if (city != "")
+        {
+            from += city;
+        }
+        window.open("route.html" + from, "route", "width=900,height=700,location=no,menubar=no,resizable=no,toolbar=no,status=no");
+    }
+}
